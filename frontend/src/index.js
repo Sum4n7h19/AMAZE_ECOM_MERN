ef1f3ff5dd2301985bde8383ad62388ed6bdb5ea
SUMANTH M <133026914+Sum4n7h19@users.noreply.github.com>
2024-01-22 23:18:26 +0530

    Fouth commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,8 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from "./store";
 import "./assests/styles/bootstrap.custom.css";
 import "./assests/styles/index.css";
 import App from "./App";
@@ -26,7 +28,9 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
